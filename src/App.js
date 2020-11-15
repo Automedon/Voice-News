30f0fd8f84458a9a9122c825f3048dddd7eaef67
Automedon <42299107+Automedon@users.noreply.github.com>
2020-11-15 20:24:33 +0700

    App.js infoCards

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import NewsCards from "./components/NewsCards/NewsCards";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import "./App.css";
+import useStyles from "./styles";
 
 function App() {
+  const classes = useStyles();
   const [newsArticles, setNewsArticles] = useState([]);
   useEffect(() => {
     alanBtn({
@@ -17,6 +19,13 @@ function App() {
   }, []);
   return (
     <div>
+      <div className={classes.logoContainer}>
+        <img
+          className={classes.alanLogo}
+          src="https://alan.app/voice/images/previews/preview.jpg"
+          alt="Alan logo"
+        />
+      </div>
       <NewsCards articles={newsArticles} />
     </div>
   );
